7e2a8581f09ac99fed06f46419bec0984463d878
mahajanl <lakshamima@gmail.com>
2017-12-04 10:05:23 -0500

    initial commit for spike playing with text alerts

---
+++ CareConnection2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CareConnection2.Startup))]
+namespace CareConnection2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
