e814e17a3f113b8a18e03c574e1d640b02fac4b1
mahajanl <lakshamima@gmail.com>
2017-12-04 21:20:21 -0500

    figuring out what and where to add twilio classes and info

--- CareConnection2/Controllers/HomeController.cs
+++ CareConnection2/Controllers/HomeController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
 namespace CareConnection2.Controllers
 {
     public class HomeController : Controller
@@ -27,4 +32,29 @@ namespace CareConnection2.Controllers
             return View();
         }
     }
+
+    static void Main(string[] args)
+    {
+        SendSms().Wait();
+        Console.Write("Press any key to continue.");
+        Console.ReadKey();
+    }
+
+    static async Task SendSms()
+    {
+        // Your Account SID from twilio.com/console
+        var accountSid = "ACc969090aee1bd200765f9e896c69c78e";
+        // Your Auth Token from twilio.com/console
+        var authToken = "auth_token";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = await MessageResource.CreateAsync(
+            to: new PhoneNumber("+15558675309"),
+            from: new PhoneNumber("+15017250604"),
+            body: "Hello from C#");
+
+        Console.WriteLine(message.Sid);
+    }
+
 }
